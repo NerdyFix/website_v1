347ee33da8ea0e0c829368441cf819dfd607f1d0
Jhon <nerdyfix@gmail.com>
2020-04-27 00:00:49 -0500

    front page modifications

--- src/components/header.js
+++ src/components/header.js
@@ -2,10 +2,12 @@ import { Link } from "gatsby"
 import PropTypes from "prop-types"
 import React from "react"
 
+import Logo from "../components/logo"
+
 const Header = ({ siteTitle }) => (
   <header
     style={{
-      background: `rebeccapurple`,
+      background: `white`,
       marginBottom: `1.45rem`,
     }}
   >
@@ -16,17 +18,10 @@ const Header = ({ siteTitle }) => (
         padding: `1.45rem 1.0875rem`,
       }}
     >
-      <h1 style={{ margin: 0 }}>
-        <Link
-          to="/"
-          style={{
-            color: `white`,
-            textDecoration: `none`,
-          }}
-        >
-          {siteTitle}
-        </Link>
-      </h1>
+      <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
+        <Logo />
+      </div>
+
     </div>
   </header>
 )
